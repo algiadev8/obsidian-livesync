f76d9cfe1bb91da1b47251114d5ba5c16e8c145a
algiadev8 <57441686+algiadev8@users.noreply.github.com>
2026-09-11 10:07:32 +0900

    Fix Obsidian read size normalisation

--- src/serviceModules/ServiceFileAccessImpl.ts
+++ src/serviceModules/ServiceFileAccessImpl.ts
@@ -1,5 +1,17 @@
 import { ServiceFileAccessBase } from "@vrtmrz/livesync-commonlib/compat/serviceModules/ServiceFileAccessBase";
+import type { UXFileInfo, UXFileInfoStub } from "@vrtmrz/livesync-commonlib/compat/common/types";
 import type { ObsidianFileSystemAdapter } from "./FileSystemAdapters/ObsidianFileSystemAdapter";
 
-// For now, this is just a re-export of ServiceFileAccess with the Obsidian-specific adapter type.
-export class ServiceFileAccessObsidian extends ServiceFileAccessBase<ObsidianFileSystemAdapter> {}
+export class ServiceFileAccessObsidian extends ServiceFileAccessBase<ObsidianFileSystemAdapter> {
+    override async readStubContent(stub: UXFileInfoStub): Promise<UXFileInfo | false> {
+        const file = await super.readStubContent(stub);
+        if (!file) return false;
+        return {
+            ...file,
+            stat: {
+                ...file.stat,
+                size: file.body.size,
+            },
+        };
+    }
+}
